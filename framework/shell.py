a1c2588d7d4b44734375952b0e994be083d00b9c
Johnny Brown <johnnybrown7@gmail.com>
2011-01-07 22:08:00 -0600

    use python -i shell.py for a python console with server({request:...}) loaded

---
+++ framework/shell.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import base64
+import socket
+import cPickle
+import zlib
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+
+sys.path.append("/clients")
+sys.path.append("../")
+from framework import constants
+
+from framework import helper_functions
+from framework.tools.logger import debug
+from framework.clients.ferrisweb import webforms
+
+server = helper_functions.server
