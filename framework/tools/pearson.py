dacade5a418dd096197a570d894f11697ee1a486
Johnny Brown <johny @thejohnnybrown.com>
2011-02-20 15:47:15 -0600

    first draft of correlation_between

--- framework/tools/pearson.py
+++ framework/tools/pearson.py
@@ -4,7 +4,6 @@ import cPickle
 if __name__ == "__main__":
     """for testing purposes"""
     samples_dict = cPickle.load(open('../../unittests/test_files/bv_study_samples_dict'))
-    print union_of_
     
 def union_of_otu_sets(samples_dict, samples_to_compare, desired_level):
     """Given a list of samples to compare, and at the level you wish to compare:
@@ -20,8 +19,11 @@ def union_of_otu_sets(samples_dict, samples_to_compare, desired_level):
     return l
 
 def correlation_between(sample_a, sample_b, level="genus"):
+    """Returns a float representing the pearson correlation between two samples. This number is a measure of how similar the two samples are.  """
     otus = union_otu_sets([sample_a,sample_b], level)
     vector_a = R.r.IntVector(map(number_for_otu_at(sample_a,level), otus))
+    vector_b = R.r.IntVector(map(number_for_otu_at(sample_b,level), otus))
+    return R.r.cor(vector_a, vector_b, method="pearson")[0]
 
 def number_for_otu_at(sample,level):
     def number_of(otu):
