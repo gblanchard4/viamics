8517955b4292a525951a26f08be622ad14ae6df7
Manuel Zubieta <mazubieta@gmail.com>
2011-02-20 15:39:24 -0600

    added uion_of_otu_sets to pearson.py

--- framework/tools/pearson.py
+++ framework/tools/pearson.py
@@ -1,4 +1,23 @@
 import rpy2.robjects as R
+import cPickle
+
+if __name__ == "__main__":
+    """for testing purposes"""
+    samples_dict = cPickle.load(open('../../unittests/test_files/bv_study_samples_dict'))
+    print union_of_
+    
+def union_of_otu_sets(samples_dict, samples_to_compare, desired_level):
+    """Given a list of samples to compare, and at the level you wish to compare:
+        
+        Return an ordered list of otu's that appear in any of the samples."""
+        
+    otus = set()
+    for sample in samples_to_compare:
+        for otu in samples_dict[sample][desired_level]:
+            otus.add(otu)
+    l = list(otus)
+    l.sort()
+    return l
 
 def correlation_between(sample_a, sample_b, level="genus"):
     otus = union_otu_sets([sample_a,sample_b], level)
